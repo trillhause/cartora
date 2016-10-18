05ebe86e51cd53af58aec357c06ce11da08dc572
Millin Gabani <gabanimillin@gmail.com>
2016-10-17 21:31:57 -0400

    Add active_model_serialization and rackCORS

--- Gemfile
+++ Gemfile
@@ -7,6 +7,10 @@ gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
 gem 'sqlite3'
 # Use Puma as the app server
 gem 'puma', '~> 3.0'
+# Api gem
+gem 'active_model_serializers'
+# Cross-Origin Resource Sharing (CORS), in order to make cross-origin AJAX requests possible.
+gem 'rack-cors'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,14 @@ module Traveltime
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    
+    #Configure Cross-Origin Resource Sharing (CORS)
+    config.middleware.insert_before 0, "Rack::Cors" do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :post, :options]
+      end
+    end
+
   end
 end
