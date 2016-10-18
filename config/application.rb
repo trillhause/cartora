240824fc0e3e469110474374703ee38dc364e65e
Millin Gabani <gabanimillin@gmail.com>
2016-10-17 21:41:11 -0400

    Add API versioning

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module Traveltime
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-    
+
     #Configure Cross-Origin Resource Sharing (CORS)
     config.middleware.insert_before 0, "Rack::Cors" do
       allow do
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
+require 'api_constraints'
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # Api definition
+  namespace :api, defaults: { format: :json },
+            constraints: { subdomain: 'api' }, path: '/'  do
+    scope module: :v1,
+          constraints: ApiConstraints.new(version: 1, default: true) do
+      # We are going to list our resources here
+    end
+  end
 end
