36526d409001df46cb7c3538a22af6d262ac25c5
Millin Gabani <gabanimillin@gmail.com>
2016-10-30 16:43:52 -0400

    POST user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -4,4 +4,20 @@ class Api::V1::UsersController < ApplicationController
   def show
     respond_with User.find(params[:id])
   end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: :created, location: [:api, user]
+    else
+      render json: {errors: user.errors }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -15,8 +15,42 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       expect(user_response[:email]).to eql @user.email
     end
 
-    it "respond is successful" do
-      expect(response.status).to eql 200
+    it { should respond_with :ok }
+  end
+
+  describe "POST #create" do
+    context "when successfully created" do
+      before(:each) do
+        @user_attributes = FactoryGirl.attributes_for  :user
+        post :create, params: {user: @user_attributes}, format: :json
+      end
+
+      it "renders the json representation for the user record just created" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql @user_attributes[:email]
+      end
+
+      it { should respond_with :created }
+    end
+
+    context "when is not created" do
+      before(:each) do
+        @invalid_user_attributes = { password: "12345678",
+                                     password_confirmation: "12345678" }
+        post :create, params: {user: @invalid_user_attributes}, format: :json
+      end
+
+      it "renders an json error" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json on why the user was not created" do
+        user_response =JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { should respond_with :unprocessable_entity }
     end
   end
 end
