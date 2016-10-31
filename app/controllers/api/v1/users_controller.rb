3646bbe759575f104607ad18d587d040c1004dbf
Millin Gabani <gabanimillin@gmail.com>
2016-10-30 23:09:39 -0400

    UPDATE users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -14,6 +14,15 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+    user = User.find(params[:id])
+    if user.update(user_params)
+      render json: user, status: :ok, location: [:api, user]
+    else
+      render json: {errors: user.errors }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 require 'api_constraints'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
-  before(:each) { request.headers['Accept'] = "application/vnd.traveltime.v1"}
+  before(:each) { request.headers['Accept'] = "application/vnd.traveltime.v1 ,  #{Mime[:json]}"}
+  before(:each) { request.headers['Content-Type'] = Mime[:json].to_s }
 
   describe "GET #show" do
     before(:each) do
@@ -22,7 +23,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     context "when successfully created" do
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for  :user
-        post :create, params: {user: @user_attributes}, format: :json
+        post :create, params: {user: @user_attributes}, format: :user
       end
 
       it "renders the json representation for the user record just created" do
@@ -37,7 +38,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       before(:each) do
         @invalid_user_attributes = { password: "12345678",
                                      password_confirmation: "12345678" }
-        post :create, params: {user: @invalid_user_attributes}, format: :json
+        post :create, params: {user: @invalid_user_attributes}
       end
 
       it "renders an json error" do
@@ -46,11 +47,47 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       end
 
       it "renders the json on why the user was not created" do
-        user_response =JSON.parse(response.body, symbolize_names: true)
+        user_response = JSON.parse(response.body, symbolize_names: true)
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
 
       it { should respond_with :unprocessable_entity }
+    end
+  end
+
+  describe "PUT/PATCH #update" do
+    context 'when successfully updated' do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, params: {id: @user.id, user: {email: 'newmail@example.com'}}
+      end
+
+      it "renders the updated json representaion of the user" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:email]).to eql 'newmail@example.com'
+      end
+
+      it { should respond_with :ok }
+    end
+
+    context 'when not updated' do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        patch :update, params: {id: @user.id, user: {email: 'bademail.com'}}
+      end
+
+      it "renders an errors json" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response).to have_key(:errors)
+      end
+
+      it "renders the json on why the user is not updated" do
+        user_response = JSON.parse(response.body, symbolize_names: true)
+        expect(user_response[:errors][:email]).to include "is invalid"
+      end
+
+      it { should respond_with :unprocessable_entity }
+
     end
   end
 end
