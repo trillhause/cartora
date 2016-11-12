be442647de40aebc3b1e754fd9b7231c78b68fd8
Millin Gabani <gabanimillin@gmail.com>
2016-11-12 14:29:05 -0500

    Temporary remove the api subdomain constraint

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'api_constraints'
 Rails.application.routes.draw do
   devise_for :users
   # Api definition
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/'  do
+  namespace :api, defaults: { format: :json }, path: '/'  do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here
       resources :users, only: [:show, :create, :update, :destroy]
