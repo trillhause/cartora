f84952f2020e7bebf55e1743b17470c4e772cc20
Millin Gabani <gabanimillin@gmail.com>
2016-11-13 00:08:11 -0500

    add check for user's auth_token and update the specs for user controller (#5)

--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::UsersController, type: :controller do
 
-  describe "GET #show" do
+  describe 'GET #show' do
     before(:each) do
         @user = FactoryGirl.create :user
         get :show, params: {id: @user.id}, format: :json
       end
 
-    it "returns the information about a reporter on a hash" do
+    it 'returns the information about a reporter on a hash' do
       user_response = JSON.parse(response.body, symbolize_names: true)
       expect(user_response[:email]).to eql @user.email
     end
@@ -16,14 +16,14 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     it { should respond_with :ok }
   end
 
-  describe "POST #create" do
-    context "when successfully created" do
+  describe 'POST #create' do
+    context 'when successfully created' do
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for  :user
         post :create, params: {user: @user_attributes}
       end
 
-      it "renders the json representation for the user record just created" do
+      it 'renders the json representation for the user record just created' do
         user_response = json_response
         expect(user_response[:email]).to eql @user_attributes[:email]
       end
@@ -31,19 +31,19 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       it { should respond_with :created }
     end
 
-    context "when is not created" do
+    context 'when is not created' do
       before(:each) do
         @invalid_user_attributes = { password: "12345678",
                                      password_confirmation: "12345678" }
         post :create, params: {user: @invalid_user_attributes}
       end
 
-      it "renders an json error" do
+      it 'renders an json error' do
         user_response = json_response
         expect(user_response).to have_key(:errors)
       end
 
-      it "renders the json on why the user was not created" do
+      it 'renders the json on why the user was not created' do
         user_response = json_response
         expect(user_response[:errors][:email]).to include "can't be blank"
       end
@@ -52,7 +52,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     end
   end
 
-  describe "PUT/PATCH #update" do
+  describe 'PUT/PATCH #update' do
     context 'when successfully updated' do
       before(:each) do
         @user = FactoryGirl.create :user
@@ -60,9 +60,9 @@ RSpec.describe Api::V1::UsersController, type: :controller do
         patch :update, params: {id: @user.id, user: {email: 'newmail@example.com'}}
       end
 
-      it "renders the updated json representaion of the user" do
+      it 'renders the updated json representaion of the user' do
         user_response = json_response
-        expect(user_response[:email]).to eql 'newmail@example.com'
+        expect(user_response[:email]).to eql "newmail@example.com"
       end
 
       it { should respond_with :ok }
@@ -75,27 +75,69 @@ RSpec.describe Api::V1::UsersController, type: :controller do
         patch :update, params: {id: @user.id, user: {email: 'bademail.com'}}
       end
 
-      it "renders an errors json" do
+      it 'renders an errors json' do
         user_response = json_response
         expect(user_response).to have_key(:errors)
       end
 
-      it "renders the json on why the user is not updated" do
+      it 'renders the json on why the user is not updated' do
         user_response = json_response
         expect(user_response[:errors][:email]).to include "is invalid"
       end
 
       it { should respond_with :unprocessable_entity }
     end
+
+    context 'when incorrect auth_token is provided' do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        api_authorization_header @user.auth_token
+        patch :update, params: {id: @user.id - 1, user: {email: 'newemail@gmail.com'}}
+      end
+
+      it 'renders an json error' do
+        user_response = json_response
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'renders the json on why the user is not updated' do
+        user_response = json_response
+        expect(user_response[:errors]).to include "Not authenticated"
+      end
+
+      it { should respond_with :unauthorized }
+    end
   end
 
-  describe "DELETE #destroy" do
-    before(:each) do
-      @user = FactoryGirl.create :user
-      api_authorization_header @user.auth_token
-      delete :destroy, params: {id: @user.id}
+  describe 'DELETE #destroy' do
+    context 'when incorrect auth_token is provided'  do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        api_authorization_header @user.auth_token
+        delete :destroy, params: {id: @user.id}
+      end
+
+      it {should respond_with :no_content}
     end
 
-    it {should respond_with :no_content}
+    context 'when incorrect auth_token is provided' do
+      before(:each) do
+        @user = FactoryGirl.create :user
+        api_authorization_header @user.auth_token
+        delete :destroy, params: {id: @user.id - 1}
+      end
+
+      it 'renders an json error' do
+        user_response = json_response
+        expect(user_response).to have_key(:errors)
+      end
+
+      it 'renders the json on why the user is not updated' do
+        user_response = json_response
+        expect(user_response[:errors]).to include "Not authenticated"
+      end
+
+      it { should respond_with :unauthorized }
+    end
   end
 end
