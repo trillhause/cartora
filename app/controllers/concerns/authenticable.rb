c5d711fe7f7f93e09ba9b68642f15eed3e870031
Millin Gabani <gabanimillin@gmail.com>
2016-11-13 00:01:38 -0500

    add check for user's auth_token and update the specs for user controller

--- app/controllers/concerns/authenticable.rb
+++ app/controllers/concerns/authenticable.rb
@@ -2,7 +2,9 @@ module Authenticable
 
   # Devise methods overwrites
   def current_user
-    @current_user ||= User.find_by(auth_token: request.headers['Authorization'])
+    @current_user ||= if User.find_by(auth_token: request.headers['Authorization'])
+      User.find_by(id: params[:id])
+    end
   end
 
   def authenticate_with_token!
