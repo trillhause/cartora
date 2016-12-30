7ff14c9915be93645b75caea415fed03e187fd63
Millin Gabani <gabanimillin@gmail.com>
2016-12-29 21:32:47 -0500

    remove nesting from users

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -33,7 +33,6 @@ class Api::V1::UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    params.permit(:first_name, :last_name, :email, :password, :password_confirmation)
   end
-
 end
--- spec/controllers/api/v1/users_controller_spec.rb
+++ spec/controllers/api/v1/users_controller_spec.rb
@@ -19,7 +19,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
     context 'when successfully created' do
       before(:each) do
         @user_attributes = FactoryGirl.attributes_for  :user
-        post :create, params: {user: @user_attributes}
+        post :create, params: @user_attributes
       end
 
       it 'renders the json representation for the user record just created' do
@@ -34,7 +34,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       before(:each) do
         @invalid_user_attributes = { password: "12345678",
                                      password_confirmation: "12345678" }
-        post :create, params: {user: @invalid_user_attributes}
+        post :create, params: @invalid_user_attributes
       end
 
       it 'renders an json error' do
@@ -56,7 +56,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       before(:each) do
         @user = FactoryGirl.create :user
         api_authorization_header @user.auth_token
-        patch :update, params: {id: @user.id, user: {email: 'newmail@example.com'}}
+        patch :update, params: {id: @user.id, email: 'newmail@example.com'}
       end
 
       it 'renders the updated json representaion of the user' do
@@ -71,7 +71,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       before(:each) do
         @user = FactoryGirl.create :user
         api_authorization_header @user.auth_token
-        patch :update, params: {id: @user.id, user: {email: 'bademail.com'}}
+        patch :update, params: {id: @user.id, email: 'bademail.com'}
       end
 
       it 'renders an errors json' do
@@ -91,7 +91,7 @@ RSpec.describe Api::V1::UsersController, type: :controller do
       before(:each) do
         @user = FactoryGirl.create :user
         api_authorization_header @user.auth_token
-        patch :update, params: {id: @user.id - 1, user: {email: 'newemail@gmail.com'}}
+        patch :update, params: {id: @user.id - 1, email: 'newemail@gmail.com'}
       end
 
       it 'renders an json error' do
